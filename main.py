6babe1a82a8f7ca54100572b0afdc7a38a3a68c8
amitavitan <avamit@post.bgu.ac.il>
2022-06-16 00:50:23 +0300

    first

--- main.py
+++ main.py
@@ -5,6 +5,10 @@ import streamlit as st
 data = pd.read_csv("healthy_lifestyle_city_2021.csv")
 st.set_page_config(page_title='Tomi', initial_sidebar_state='expanded', layout="wide", page_icon="running")
 st.dataframe(data)
-st.write(data.columns)
+# st.write(data.columns)
 
-st.bar_chart(data[['City', 'Sunshine hours(City)']])
+# st.bar_chart(data[['City', 'Sunshine hours(City)']])
+columns = data.columns.tolist()
+
+selected_columns = st.multiselect("select column", columns, default="location")
+s = data[selected_columns[0]].str.strip().value_counts()
