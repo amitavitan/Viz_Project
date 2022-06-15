2e7d91d83350579b60de0bb35f24b9ecea10dbfb
amitavitan <avamit@post.bgu.ac.il>
2022-06-16 00:43:19 +0300

    first

--- main.py
+++ main.py
@@ -5,6 +5,6 @@ import streamlit as st
 data = pd.read_csv("healthy_lifestyle_city_2021.csv")
 st.set_page_config(page_title='Tomi', initial_sidebar_state='expanded', layout="wide", page_icon="running")
 st.dataframe(data)
+st.write(data.columns)
 
-
-st.bar_chart(data)
+# st.bar_chart(data[])
