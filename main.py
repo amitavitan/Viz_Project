b4f3b4b7d0e84879fdd6250f5b759ae5235d78d2
amitavitan <avamit@post.bgu.ac.il>
2022-06-16 00:38:52 +0300

    first

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ import streamlit as st
 
 
 data = pd.read_csv("healthy_lifestyle_city_2021.csv")
-st.set_page_config(page_title='Tomi', initial_sidebar_state='expanded', layout="wide")
+st.set_page_config(page_title='Tomi', initial_sidebar_state='expanded', layout="wide", page_icon="🏃🏻‍")
 st.dataframe(data)
