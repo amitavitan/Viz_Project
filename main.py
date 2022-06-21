0fe6868c29bb8470f8db076180e8961c4a1f15cb
amitavitan <avamit@post.bgu.ac.il>
2022-06-21 18:42:25 +0300

    first

--- main.py
+++ main.py
@@ -15,16 +15,16 @@ data['Obesity levels(Country)'] = 0.01 * (data['Obesity levels(Country)'].str.re
 st.set_page_config(page_title='Tomi', initial_sidebar_state='expanded', layout="wide", page_icon="running")
 st.dataframe(data)
 # st.write(data.columns)
-chart_data = pd.DataFrame(
-    data['City'],
-    columns=["a", "b", "c"])
-# st.bar_chart(data[['City', 'Sunshine hours(City)']])
-selected_ciry = st.radio('Pick one', list(data['City']), horizontal=True)
-if selected_ciry == 'Tel Aviv':
-    st.write('You selected Tel Aviv.')
-    st.bar_chart(data['Sunshine hours(City)'])
-else:
-    st.write("You didn't select comedy.")
+# chart_data = pd.DataFrame(
+#     data['City'],
+#     columns=["a", "b", "c"])
+# # st.bar_chart(data[['City', 'Sunshine hours(City)']])
+# selected_ciry = st.radio('Pick one', list(data['City']), horizontal=True)
+# if selected_ciry == 'Tel Aviv':
+#     st.write('You selected Tel Aviv.')
+#     st.bar_chart(data['Sunshine hours(City)'])
+# else:
+#     st.write("You didn't select comedy.")
 
 # st.bar_chart(chart_data)
 # @Email:  contact@pythonandvba.com
