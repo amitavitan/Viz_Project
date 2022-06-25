4dcbcc498b63d56058b1d81416475d8f4a9b4fcd
emilyBGU <emilyt@post.bgu.ac.il>
2022-06-25 19:41:33 +0300

    c

--- main.py
+++ main.py
@@ -46,7 +46,33 @@ columns = list(df.columns)
 columns.remove("City")
 columns.remove("Rank")
 
-st.dataframe(df)
+
+
+
+# st.dataframe(df)
+
+####################
+### INTRODUCTION ###
+####################
+
+# ---- MAINPAGE ----
+row0_spacer1, row0_1, row0_spacer2, row0_2, row0_spacer3 = st.columns((.1, 2.3, .1, 1.3, .1))
+with row0_1:
+    st.title(':bar_chart: Lifestyle Dashboard - Healthy Habits :bar_chart:')
+    st.markdown("##")
+with row0_2:
+    st.text("")
+    st.subheader('Streamlit App by [Tim Denzler](https://www.linkedin.com/in/tim-denzler/)')
+row3_spacer1, row3_1, row3_spacer2 = st.columns((.1, 3.2, .1))
+with row3_1:
+    st.markdown("Hello there! Have you ever spent your weekend watching the German Bundesliga and had your friends complain about how 'players definitely used to run more' ? However, you did not want to start an argument because you did not have any stats at hand? Well, this interactive application containing Bundesliga data from season 2013/2014 to season 2019/2020 allows you to discover just that! If you're on a mobile device, I would recommend switching over to landscape for viewing ease.")
+    st.markdown("You can find the source code in the [BuLiAn GitHub Repository](https://github.com/tdenzl/BuLiAn)")
+    st.markdown("If you are interested in how this app was developed check out my [Medium article](https://tim-denzler.medium.com/is-bayern-m%C3%BCnchen-the-laziest-team-in-the-german-bundesliga-770cfbd989c7)")
+
+
+####################
+### SELECTION ###
+####################
 
 # ---- SIDEBAR ----
 st.sidebar.header("Please Filter Here:")
@@ -57,10 +83,6 @@ city = st.sidebar.multiselect(
 )
 
 df_selection = df.query("City == @city")
-
-# ---- MAINPAGE ----
-st.title(":bar_chart: Healthy Lifestyle Dashboard")
-st.markdown("##")
 
 for i, place in enumerate(st.columns(int(len(columns)/2))):
     with place:
