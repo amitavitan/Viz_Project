381fc5e45f17b5847033fa9c9ed3f0289cb4060f
emilyBGU <emilyt@post.bgu.ac.il>
2022-06-26 00:53:53 +0300

    c

--- main.py
+++ main.py
@@ -155,6 +155,8 @@ with row7_1:
     y = list(df_corr.index)
     z = np.array(df_corr)
     fig_corr_matrix = ff.create_annotated_heatmap(z, x=x, y=y, annotation_text=np.around(z, decimals=2), hoverinfo='z', colorscale='RdPu', showscale=True)
+    fig_corr_matrix.update_traces(textposition='auto', textfont=dict(color='#fff'))
+
     st.plotly_chart(fig_corr_matrix, use_container_width=False)
 with row7_2:
     st.subheader('Correlation Of Attributes')
