499f7fb508da3f4ad1b59d28c601d96d556930e5
emilyBGU <emilyt@post.bgu.ac.il>
2022-06-25 19:49:11 +0300

    c

--- main.py
+++ main.py
@@ -62,18 +62,17 @@ with row0_1:
     st.markdown("##")
 with row0_2:
     st.text("")
-    st.subheader('Streamlit App by [Tim Denzler](https://www.linkedin.com/in/tim-denzler/)')
+    st.subheader('Streamlit App by Amit Avitan & Emily Toyber')
 row3_spacer1, row3_1, row3_spacer2 = st.columns((.1, 3.2, .1))
 with row3_1:
-    st.markdown("Hello there! Have you ever spent your weekend watching the German Bundesliga and had your friends complain about how 'players definitely used to run more' ? However, you did not want to start an argument because you did not have any stats at hand? Well, this interactive application containing Bundesliga data from season 2013/2014 to season 2019/2020 allows you to discover just that! If you're on a mobile device, I would recommend switching over to landscape for viewing ease.")
-    st.markdown("You can find the source code in the [BuLiAn GitHub Repository](https://github.com/tdenzl/BuLiAn)")
-    st.markdown("If you are interested in how this app was developed check out my [Medium article](https://tim-denzler.medium.com/is-bayern-m%C3%BCnchen-the-laziest-team-in-the-german-bundesliga-770cfbd989c7)")
+    st.markdown("first paragraph - introduction")
+    st.markdown("second paragraph - explanation and github repo")
 
 
 ####################
 ### SELECTION ###
 ####################
-# row2_spacer1, row2_1, row2_spacer2, row2_2, row2_spacer3, row2_3, row2_spacer4, row2_4, row2_spacer5   = st.columns((.2, 1.6, .2, 1.6, .2, 1.6, .2, 1.6, .2))
+# row2_spacer1, row2_1, row2_spacer2, row2_2, row2_spacer3, row2_3, row2_spacer4, row2_4, row2_spacer5 = st.columns((.2, 1.6, .2, 1.6, .2, 1.6, .2, 1.6, .2))
 # with row2_1:
 #     unique_games_in_df = df_data_filtered.game_id.nunique()
 #     str_games = "🏟️ " + str(unique_games_in_df) + " Matches"
@@ -97,7 +96,7 @@ with row3_1:
 # row3_spacer1, row3_1, row3_spacer2 = st.columns((.2, 7.1, .2))
 # with row3_1:
 st.markdown("")
-see_data = st.expander('You can click here to see the raw data first 👉')
+see_data = st.expander('Click here to see the raw data 👉')
 with see_data:
     st.dataframe(data=df.reset_index(drop=True))
 st.text('')
