ef96e2440ca861557b792842c4e01b12146be171
emilyBGU <emilyt@post.bgu.ac.il>
2022-06-26 13:27:12 +0300

    c

--- main.py
+++ main.py
@@ -151,7 +151,7 @@ st.markdown("""---""")
 row6_spacer1, row6_1, row6_spacer2 = st.columns((.2, 7.1, .2))
 with row6_1:
     st.subheader('Correlation Of Attributes')
-row7_spacer1, row7_1, row7_spacer2, row7_2, row7_spacer3 = st.columns((.08, 4.1, .4, 3.5, .2))
+row7_spacer1, row7_1, row7_spacer2, row7_2, row7_spacer3 = st.columns((.1, 4.1, .4, 3.5, .2))
 with row7_1:
     df_corr = df_selection.corr()  # Generate correlation matrix
     fig_corr_matrix = go.Figure()
@@ -163,7 +163,7 @@ with row7_1:
     print(y)
     z = np.array(df_corr)
     fig_corr_matrix = ff.create_annotated_heatmap(z, x=x, y=y, annotation_text=np.around(z, decimals=2), hoverinfo='z', colorscale='RdPu', showscale=True)
-    fig_corr_matrix.update_layout(autosize=True, margin=dict(l=10, r=10, t=10, b=10))
+    fig_corr_matrix.update_layout(autosize=True, margin=dict(l=0, r=10, t=10, b=10))
     st.plotly_chart(fig_corr_matrix, use_container_width=True)
 with row7_2:
     st.subheader('Correlation Of Attributes')
