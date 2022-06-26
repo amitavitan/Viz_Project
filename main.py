59fafc0071a6c03b5bf8d7a3ab67f2664c7e4456
emilyBGU <emilyt@post.bgu.ac.il>
2022-06-26 17:02:54 +0300

    c

--- main.py
+++ main.py
@@ -186,6 +186,11 @@ st.markdown("""---""")
 row4_spacer1, row4_1, row4_spacer2 = st.columns((.2, 7.1, .2))
 with row4_1:
     st.subheader('Correlation Of Attributes')
+    col1 = st.selectbox("Choose lifestyle parameter to analyze:", options=columns, index=0)
+    col2 = st.selectbox("Select another lifestyle parameter to investigate the relationship:", options=columns, index=1)
+    X = df_selection[(df_selection[col1] != 0) & (df_selection[col2] != 0)]
+    title1 = clean_col_name(col1)
+    title2 = clean_col_name(col2)
 row5_spacer1, row5_1, row5_spacer2, row5_2, row5_spacer3 = st.columns((.1, 4.1, .4, 3.5, .2))
 with row5_1:
     df_corr = df_selection.corr()  # Generate correlation matrix
@@ -199,6 +204,10 @@ with row5_1:
     st.plotly_chart(fig_corr_matrix, use_container_width=True)
 with row5_2:
     st.subheader('ssdefsvf Of Attributes')
+    fig_trend = px.scatter(data_frame=df_selection, x=col1, y=col2, color="Continent",  trendline="ols")#, trendline_scope="overall")
+    fig_trend.update_traces(marker_line_width=1, marker_size=12)
+    st.plotly_chart(fig_trend, use_container_width=True)
+
     # selected_col1 = st.selectbox("Which lifestyle parameter do you want to analyze?", options=columns, index=0)
     # selected_col2 = st.selectbox("Select another lifestyle parameter to investigate the relationship:", options=columns, index=1)
     # x1 = df_selection[df_selection[selected_col1] != 0].groupby('Continent')[selected_col1].agg(['mean']).reset_index("Continent")
@@ -235,20 +244,20 @@ with row5_2:
 st.markdown("""---""")
 
 
-
-row7_spacer1, row7_1, row7_spacer2, row7_2, row7_spacer3 = st.columns((.2, 3.5, .4, 3.5, .2))
-with row7_1:
-    st.subheader('Analysis per Matchday')
-    col1 = st.selectbox("Choose lifestyle parameter to analyze:", options=columns, index=0)
-    col2 = st.selectbox("Select another lifestyle parameter to investigate the relationship:", options=columns, index=1)
-    X = df_selection[(df_selection[col1] != 0) & (df_selection[col2] != 0)]
-    title1 = clean_col_name(col1)
-    title2 = clean_col_name(col2)
-with row7_2:
-    fig_trend = px.scatter(data_frame=df_selection, x=col1, y=col2, color="Continent",  trendline="ols")#, trendline_scope="overall")
-    fig_trend.update_traces(marker_line_width=1, marker_size=12)
-    st.plotly_chart(fig_trend, use_container_width=True)
-
+#
+# row7_spacer1, row7_1, row7_spacer2, row7_2, row7_spacer3 = st.columns((.2, 3.5, .4, 3.5, .2))
+# with row7_1:
+#     st.subheader('Analysis per Matchday')
+#     col1 = st.selectbox("Choose lifestyle parameter to analyze:", options=columns, index=0)
+#     col2 = st.selectbox("Select another lifestyle parameter to investigate the relationship:", options=columns, index=1)
+#     X = df_selection[(df_selection[col1] != 0) & (df_selection[col2] != 0)]
+#     title1 = clean_col_name(col1)
+#     title2 = clean_col_name(col2)
+# with row7_2:
+#     fig_trend = px.scatter(data_frame=df_selection, x=col1, y=col2, color="Continent",  trendline="ols")#, trendline_scope="overall")
+#     fig_trend.update_traces(marker_line_width=1, marker_size=12)
+#     st.plotly_chart(fig_trend, use_container_width=True)
+#
 
 
 # ---- HIDE STREAMLIT STYLE ----
